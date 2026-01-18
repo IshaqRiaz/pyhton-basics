1583d58403462beca79965a93f62235841ce2029
Muhammad Ishaq <99590439+IshaqRiaz@users.noreply.github.com>
2026-01-18 18:34:54 +0500

    Add examples of for and while loops

---
+++ loops.py
@@ -0,0 +1,11 @@
+# Examples of for loop and while loop
+
+# For loop
+for i in range(1, 6):
+    print("For loop value:", i)
+
+# While loop
+count = 1
+while count <= 5:
+    print("While loop value:", count)
+    count += 1
